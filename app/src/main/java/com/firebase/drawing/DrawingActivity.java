26bca8ffe196768d026d5969a73523f2d54019a6
Dmitry Nefedkin <dnefedkin@gmail.com>
2014-12-10 10:27:20 +0300

    Initial commit

---
+++ app/src/main/java/com/firebase/drawing/DrawingActivity.java
@@ -0,0 +1,95 @@
+package com.firebase.drawing;
+
+import android.app.Activity;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class DrawingActivity extends Activity implements ColorPickerDialog.OnColorChangedListener {
+
+    // TODO: change this to your own Firebase URL
+    private static final String FIREBASE_URL = "https://blazing-torch-5922.firebaseio.com";
+
+    private static final int COLOR_MENU_ID = Menu.FIRST;
+
+    private DrawingView drawingView;
+    private Firebase ref;
+    private ValueEventListener connectedListener;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        ref = new Firebase(FIREBASE_URL);
+        drawingView = new DrawingView(this, ref);
+        setContentView(drawingView);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Set up a notification to let us know when we're connected or disconnected from the Firebase servers
+        connectedListener = ref.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean)dataSnapshot.getValue();
+                if (connected) {
+                    Toast.makeText(DrawingActivity.this, "Connected to Firebase", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(DrawingActivity.this, "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Clean up our listener so we don't have it attached twice.
+        ref.getRoot().child(".info/connected").removeEventListener(connectedListener);
+        drawingView.cleanup();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, COLOR_MENU_ID, 0, "Color").setShortcut('3', 'c');
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == COLOR_MENU_ID) {
+            new ColorPickerDialog(this, this, 0xFFFF0000).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void colorChanged(int newColor) {
+        drawingView.setColor(newColor);
+    }
+}
